c4df34a95f63d545dd09b4b08a012eab244e4c1c
Kamil Zakiev <zakiev-kamil@mail.ru>
2021-01-12 17:39:19 +0300

    added basic Kafka consumer example

---
+++ KafkaSamples/TempAnalyzer/MessageConsumer.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace TempAnalyzer
+{
+    internal class MessageConsumer: BackgroundService
+    {
+        private readonly ILogger<MessageConsumer> _log;
+        private readonly IConsumer<string, int> _consumer;
+
+        public MessageConsumer(ILogger<MessageConsumer> log, IConsumer<string, int> consumer)
+        {
+            _log = log;
+            _consumer = consumer;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await Task.Yield();
+            
+            var i = 0;
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var consumeResult = _consumer.Consume(stoppingToken);
+                
+                _log.LogInformation(consumeResult.Message.Key + " - " + consumeResult.Message.Value);
+                
+                if (i++ % 1000 == 0)
+                {
+                    _consumer.Commit();
+                }
+            }
+        }
+
+        public override void Dispose()
+        {
+            _consumer.Dispose();
+            base.Dispose();
+        }
+    }
+}
---
+++ KafkaSamples/TempAnalyzer/Program.cs
@@ -0,0 +1,27 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace TempAnalyzer
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((context, services) =>
+                {
+                    var consumerConfig = context.Configuration.GetSection("ConsumerConfig").Get<ConsumerConfig>();
+                    var consumer = new ConsumerBuilder<string, int>(consumerConfig).Build();
+                    consumer.Subscribe("WeatherTopic");
+
+                    services.AddHostedService(sp =>
+                        new MessageConsumer(sp.GetRequiredService<ILogger<MessageConsumer>>(), consumer));
+                })
+                .Build()
+                .Run();
+        }
+    }
+}
